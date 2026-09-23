6de20319371c8a5a99aecb4827db8c75f31c7ef6
Ronal Casani <ronalsistemas@gmail.com>
2026-09-23 17:25:49 -0500

    feat: inicializando servicio alumno

---
+++ api-alumno-service-v1/src/main/java/pe/edu/rcasani/alumno/ApiAlumnoServiceV1Application.java
@@ -0,0 +1,13 @@
+package pe.edu.rcasani.alumno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAlumnoServiceV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAlumnoServiceV1Application.class, args);
+	}
+
+}
